8d82ecb0998bf35cc954885107c2f9dee98a1a89
LynnCzyla <lynnzylameczdo@gmail.com>
2026-06-18 19:21:07 +0800

    FAVICON

--- main/src/main.jsx
+++ main/src/main.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import logo from './assets/WEA_logo_bgremoved.png'
+
+const link = document.querySelector("link[rel~='icon']") || document.createElement('link')
+link.type = 'image/png'
+link.rel = 'icon'
+link.href = logo
+document.getElementsByTagName('head')[0].appendChild(link)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
