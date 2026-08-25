343ceb9770f894d398c3daae70b60fd04651355a
mell0714 <ebuen_romell@plpasig.edu.ph>
2026-08-26 00:45:47 +0800

    Update employeeApi.js

--- backend/routes/Employee/employeeApi.js
+++ backend/routes/Employee/employeeApi.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use(require('./dashboard'));
 router.use(require('./Assignments'));
 router.use(require('./Profile'));
+router.use(require('./Feedback'));
 
 module.exports = router;
