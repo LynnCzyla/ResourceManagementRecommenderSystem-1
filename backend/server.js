c5427c3f87acf5371cd2a844a4222b0ad64967f2
mell0714 <ebuen_romell@plpasig.edu.ph>
2026-09-07 20:39:56 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 // D:\ResourceManagementRecommenderSystem\backend\server.js
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, ".env") });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,7 +14,6 @@ const recommendationEngine = require("./services/recommendationService");
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-const path = require("path");
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // ============ IMPORT AUTH MIDDLEWARE ============
